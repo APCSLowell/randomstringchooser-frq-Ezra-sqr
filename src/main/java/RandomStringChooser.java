3c42570ad54456e3dd88a2f2a786822073aa16a8
Ezra-sqr <143028029+Ezra-sqr@users.noreply.github.com>
2024-04-23 20:39:22 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,13 +1,13 @@
 import java.util.*;
 public class RandomStringChooser
 {
-  private ArrayList <String> myArray;
-  public void randomStringChooser(string[] wordArray){
-   myArray=new ArrayList<String>();
+  private arrayList <String> myArray;
+  public void randomStringChooser(String[] wordArray){
+   myArray=new arrayList<String>();
    for (int i=0; i<wordArray.length; i++)
      myArray.add(i);
   }
-  public string getNext(){
+  public String getNext(){
   if (myArray.size()>0)
     return myArray.remove(Math.random()*myArray.Size());
   return -1;
